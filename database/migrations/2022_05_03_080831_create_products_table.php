b5ac3cf25c27447ff6a7d99e99702d604ecab096
Huy20102002 <huy.dev.2010@gmail.com>
2022-05-03 15:45:08 +0700

    Laravel: setup

---
+++ database/migrations/2022_05_03_080831_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('price');
+            $table->text('description');
+            $table->integer('idCate');
+            $table->integer("status");
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
