a4aeff15fb30a37750c19fc7af3f0d8cec9cb6ce
Huy20102002 <huy.dev.2010@gmail.com>
2022-05-05 22:39:13 +0700

    Slide:Finish Api

---
+++ app/Http/Controllers/SlideController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Slider;
+use Illuminate\Http\Request;
+
+class SlideController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $all = Slider::all();
+        return response()->json(['data' => $all]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:6',
+            'image' => 'required'
+        ]);
+        $model = new Slider();
+        $model->name = $request->name;
+        $model->image = $request->image;
+        $model->save();
+        return response()->json(200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Slider::find($id);
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|min:6',
+            'image' => 'required'
+        ]);
+        $model = Slider::find($id);
+        $model->name = $request->name;
+        $model->image = $request->image;
+        $model->save();
+        return response()->json(200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (!($slide = Slider::find($id))) return abort(404);
+        $slide->delete();
+        return redirect()->back();
+    }
+}
